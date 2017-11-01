5f55f522d529d5897bc6a7bed82dc35bcfba2b0c
Jan Miderbäck <jan.miderback@gmail.com>
2017-11-01 16:13:49 +0100

    Restored/new banner image (#60)

--- serve.sh
+++ serve.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 set -e
 cd "$(dirname "$0")"
+
+MKDOCS_IMAGE=squidfunk/mkdocs-material
+
 if [[ "$OS" == "Windows_NT" ]]; then
-    WINPTY=winpty
+  winpty docker run --rm -it -p 8000:8000 -v /`pwd`://docs $MKDOCS_IMAGE
+else
+  docker run --rm -it -p 8000:8000 -v `pwd`:/docs $MKDOCS_IMAGE
 fi
-$WINPTY docker run --rm -it -p 8000:8000 -v /`pwd`://docs squidfunk/mkdocs-material
