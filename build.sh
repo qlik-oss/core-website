ab8089aaf36ff96d5fe87062cb583903a7665f41
Jan Miderbäck <jan.miderback@qlik.com>
2017-11-01 09:10:56 +0100

    Use machine exec with volumes

--- build.sh
+++ build.sh
@@ -7,9 +7,7 @@ MKDOCS_IMAGE=squidfunk/mkdocs-material
 echo "Building site into `pwd`/site/ folder."
 
 if [[ "$OS" == "Windows_NT" ]]; then
-  echo "OS is Windows."
   winpty docker run --rm -it -v /`pwd`://docs $MKDOCS_IMAGE build
 else
-  echo "OS is Unix."
   docker run --rm -it -v `pwd`:/docs $MKDOCS_IMAGE build
 fi
