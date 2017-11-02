6e79083818fbb672a210f52fac80981254a82fc5
Mattias Wennmo <mow@qlik.com>
2017-11-02 10:59:02 +0100

    Update after PR comments

--- lint.sh
+++ lint.sh
@@ -2,10 +2,4 @@
 set -e
 cd "$(dirname "$0")"
 
-LINT_CONDO_IMAGE=singapore/lint-condo
-
-if [[ "$OS" == "Windows_NT" ]]; then
-  docker run -v /`pwd`:/src/ $LINT_CONDO_IMAGE
-else
-  docker run -v `pwd`:/src/ $LINT_CONDO_IMAGE
-fi
+docker run -v /`pwd`:/src/ singapore/lint-condo
