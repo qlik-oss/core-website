75cdb0edf7c0c70d1af326fd8f08cf47beb328c6
Mattias Wennmo <wennmo@users.noreply.github.com>
2017-11-03 15:46:15 +0100

    Remove lint container after exit (#75)

--- lint.sh
+++ lint.sh
@@ -2,4 +2,4 @@
 set -e
 cd "$(dirname "$0")"
 
-docker run -v /`pwd`/docs/:/src/ -v /`pwd`/docs/.proselintrc:/root/.proselintrc singapore/lint-condo
+docker run --rm -v /`pwd`/docs/:/src/ -v /`pwd`/docs/.proselintrc:/root/.proselintrc singapore/lint-condo
