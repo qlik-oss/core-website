d6066f49ba05e047cc69a84990a47ec196244668
Fredrik Folkesson <Fredrik.Folkesson@qlik.com>
2017-11-06 15:24:02 +0100

    Changed on how we get the current directory on windows to use Windows Paths (#83)

--- lint.sh
+++ lint.sh
@@ -2,4 +2,8 @@
 set -e
 cd "$(dirname "$0")"
 
-docker run --rm -v /`pwd`/docs/:/src/ -v /`pwd`/docs/.proselintrc:/root/.proselintrc singapore/lint-condo
+if [[ "$OS" == "Windows_NT" ]]; then
+  docker run --rm -v `pwd -W`/docs/:/src/ -v /`pwd -W`/docs/.proselintrc:/root/.proselintrc singapore/lint-condo
+else
+  docker run --rm -v /`pwd`/docs/:/src/ -v /`pwd`/docs/.proselintrc:/root/.proselintrc singapore/lint-condo
+fi
