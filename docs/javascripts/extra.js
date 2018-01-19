0f526b3ab1c5706a52e1ea2fb070568728375922
Helene Rigner <hvs@qlik.com>
2018-01-19 14:13:40 +0100

    Fixing js error

--- docs/javascripts/extra.js
+++ docs/javascripts/extra.js
@@ -1,6 +1,4 @@
-if (location.pathname == "/") {
-  if (document.getElementById("marketing-page")) {
-    document.getElementsByClassName("md-content")[0].className +=
-      " marketing-page-main-content";
-  }
+if (document.getElementById("marketing-page")) {
+  document.getElementsByClassName("md-content")[0].className +=
+    " marketing-page-main-content";
 }
