97150ffafef351564f818f2d698c1035260fe2ef
Helene Rigner <hvs@qlik.com>
2018-01-23 07:49:19 +0100

    Added slack icon to footer. Updated footer design.

--- docs/javascripts/extra.js
+++ docs/javascripts/extra.js
@@ -1,21 +1,19 @@
 if (document.getElementById("marketing-page")) {
   document.getElementsByClassName("md-content")[0].className +=
     " marketing-page-main-content";
-  
+
   var elems = document.querySelectorAll(".md-content__inner");
   elems.forEach(function(element) {
     var children = element.children;
     var i = 0;
     var nbrOfChildrens = element.childElementCount;
-    while(nbrOfChildrens > 0){
-      if(children[i].id != "marketing-page") {
+    while (nbrOfChildrens > 0) {
+      if (children[i].id != "marketing-page") {
         children[i].remove();
-      }
-      else {
+      } else {
         i++;
       }
       nbrOfChildrens--;
-    } 
+    }
   });
 }
-
