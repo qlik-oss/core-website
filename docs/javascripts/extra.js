0782aa5dc205d4dae6997ba27d18ef7a5a739764
Helene Rigner <hvs@qlik.com>
2018-01-25 15:52:55 +0100

    - Changed toc title. - Removed js to hide edit link on homepage in favor of doing it in html.

--- docs/javascripts/extra.js
+++ docs/javascripts/extra.js
@@ -3,7 +3,5 @@
   if (document.getElementById("marketing-page")) {
     document.getElementsByClassName("md-content")[0].className +=
       " marketing-page-main-content";
-    document.getElementsByClassName("md-icon md-content__icon")[0].className +=
-      " hidden";
   }
 })();
