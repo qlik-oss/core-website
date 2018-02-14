ff6d31cb383f9bde66cf820584b7052f0d4684fe
Mattias Wennmo <mow@qlik.com>
2018-02-14 13:32:01 +0100

    Fix bash scripts for windows

--- lint.sh
+++ lint.sh
@@ -2,10 +2,13 @@
 set -e
 cd "$(dirname "$0")"
 
+docker_cmd=docker
+pwd=$(pwd)
+
 if [[ "$OS" == "Windows_NT" ]]; then
+  docker_cmd="winpty docker"
   pwd=/$(pwd -W)
-else
-  pwd=$(pwd)
 fi
 
-docker run --rm -v $pwd/docs/:/src/ -v $pwd/docs/.proselintrc:/root/.proselintrc -it singapore/lint-condo:0.18.0
+
+$docker_cmd run --rm -v $pwd/docs/:/src/ -v $pwd/docs/.proselintrc:/root/.proselintrc -it singapore/lint-condo:0.18.0
