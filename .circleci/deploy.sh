949ee9d57fc51e11880febc8bbadb640e85c7703
Håkan Axelsson <han@qlik.com>
2018-04-13 08:03:06 +0200

    Adding script for post comment with URL back to Github

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -44,3 +44,10 @@ ssh $ssh_opts $target_machine "cd docs && ln -sfn ./$BUILD_NUMBER ./$latest_name
 
 echo "Atomic URL: https://$TARGET_HOST/docs/$BUILD_NUMBER"
 echo "Latest URL: https://$TARGET_HOST/docs/$latest_name"
+
+curl -u none:$GH_TOKEN https://api.github.com/repos/qlik-oss/core-website/pulls --request POST --data "{ 
+  \"title\": \"Automated: Qlik Core website preview\",
+  \"body\": \"Hello! The changes to Qlik Core website has been deployed for easy review.\n\nAtomic URL: https://$TARGET_HOST/docs/$BUILD_NUMBER\",
+  \"head\": \"$BRANCH_NAME\",
+  \"base\": \"master\"
+}"
