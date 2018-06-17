3690d4909d044d60d895dbfffa4e78657129c37d
Johan Bjerning <johan@bjerning.se>
2018-06-17 22:58:21 +0200

    Clean up of old marketing page and comments, added new branding

--- docs/javascripts/extra.js
+++ docs/javascripts/extra.js
@@ -1,7 +1,19 @@
-// The start page is shown in full page
-(function() {
-  if (document.getElementById("marketing-page")) {
-    document.getElementsByClassName("md-content")[0].className +=
-      " marketing-page-main-content";
+function cookiePopup() {
+  if (document.getElementById("cookieModal")){
+    document.getElementsByClassName("accept-cookies")[0].className +=
+    " hidden";}
   }
+
+  
+(function() { 
+  var imgCount = 5;
+  var dir = '/images/';
+  var randomCount = Math.round(Math.random() * (imgCount - 1)) + 1;
+  var images = new Array
+          images[1] = "bg-1.jpg",
+          images[2] = "bg-2.jpg",
+          images[3] = "bg-3.jpg",
+          images[4] = "bg-4.jpg",
+          images[5] = "bg-5.jpg"
+  document.getElementById("header-id").style.backgroundImage = "url(" + dir + images[randomCount] + ")"; 
 })();
