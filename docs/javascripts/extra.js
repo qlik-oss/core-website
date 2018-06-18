efbf2d6ea258029ff8c3a8a159baed93b9d32e4c
Johan Bjerning <johan@bjerning.se>
2018-06-18 22:22:58 +0200

    Clean-up and fix for bg files

--- docs/javascripts/extra.js
+++ docs/javascripts/extra.js
@@ -7,7 +7,7 @@ function cookiePopup() {
   
 (function() { 
   var imgCount = 5;
-  var dir = '/images/';
+  var dir = document.location + 'images/';
   var randomCount = Math.round(Math.random() * (imgCount - 1)) + 1;
   var images = new Array
           images[1] = "bg-1.jpg",
@@ -16,15 +16,4 @@ function cookiePopup() {
           images[4] = "bg-4.jpg",
           images[5] = "bg-5.jpg"
   document.getElementById("header-id").style.backgroundImage = "url(" + dir + images[randomCount] + ")"; 
-})();
-
-(function() { 
-  var h1 = document.getElementsByTagName("h1");
-  var node = document.createElement("SPAN");                 // Create a <li> node
-  var textnode = document.createTextNode("/ "); 
-  node.appendChild(textnode);
-  for (i = 0; i < h1.length; i++) {
-    h1[i].insertAdjacentHTML('afterbegin', '<span>/ </span>');
-    //console.log(h1[i]); //.parentNode.insertBefore(node, h1[i]);
-  }
 })();
