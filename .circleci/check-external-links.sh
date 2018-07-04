5893f2b1d6855d35c37dce017776fab69f85c1ab
Henrik Carlioth <hca@qlik.com>
2018-07-04 13:44:39 +0200

    Check for new external links, and check if broken.

---
+++ .circleci/check-external-links.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+# Select the diff from the branch. Grep the changes indicated with the '+' sign. Use grep with extended regex syntax to identify weblinks. Trim text with sed.
+URLS=`curl -s -L "https://github.com/qlik-oss/core-website/compare/$CIRCLE_JOB.diff" | grep '+' | grep -Eo '\(https?://[^ ]+\)' | sed 's/^.\(.*\).$/\1/' | uniq`
+
+for url in $URLS
+do 
+  if curl --output /dev/null --silent --head --fail "$url"; then
+    echo "Working links $url"
+  else
+    echo "The following link is broken: $url"
+    exit 1
+  fi 
+done
