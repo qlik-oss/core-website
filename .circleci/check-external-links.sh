be9e144a954854e1f0398c4b07ba7aac9385bad6
Henrik Carlioth <hca@qlik.com>
2018-07-04 15:04:08 +0200

    Better fail check, and dollar expansion instead

--- .circleci/check-external-links.sh
+++ .circleci/check-external-links.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 set -e
-
+STATUS=0
 # Select the diff from the branch. Grep the changes indicated with the '+' sign. Use grep with extended regex syntax to identify weblinks. Trim text with sed.
-URLS=`curl -s -L "https://github.com/qlik-oss/core-website/compare/$CIRCLE_BRANCH.diff" | grep '+' | grep -Eo '\(https?://[^ ]+\)' | sed 's/^.\(.*\).$/\1/' | sed 's/)//g' | uniq`
+URLS=$(curl -s -L "https://github.com/qlik-oss/core-website/compare/$CIRCLE_BRANCH.diff" | grep '+' | grep -Eo '\(https?://[^ ]+\)' | sed 's/^.\(.*\).$/\1/' | sed 's/)//g' | uniq)
 echo "Following URLS found: $URLS"
 
 for url in $URLS
@@ -12,6 +12,10 @@ do
     echo "Working links $url"
   else
     echo "The following link is broken: $url"
-    exit 1
+    STATUS=1
   fi 
 done
+
+if [ "$STATUS" -eq "1" ]; then
+   exit;
+fi
