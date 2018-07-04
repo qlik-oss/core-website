3c77350dc05ccfb62da99caf129bdd11f28c83b1
Henrik Carlioth <hca@qlik.com>
2018-07-04 14:27:51 +0200

    More info, and validation change

--- .circleci/check-external-links.sh
+++ .circleci/check-external-links.sh
@@ -4,6 +4,7 @@ set -e
 
 # Select the diff from the branch. Grep the changes indicated with the '+' sign. Use grep with extended regex syntax to identify weblinks. Trim text with sed.
 URLS=`curl -s -L "https://github.com/qlik-oss/core-website/compare/$CIRCLE_BRANCH.diff" | grep '+' | grep -Eo '\(https?://[^ ]+\)' | sed 's/^.\(.*\).$/\1/' | sed 's/)//g' | uniq`
+echo "Following URLS found: $URLS"
 
 for url in $URLS
 do 
