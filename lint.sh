021ef1c8022ba2f016a49985e07cab6b10a5e6fa
Jonas Hörström <Jonas.Horstrom@qlik.com>
2018-07-10 13:15:03 +0200

    Removing mingw specific code and adding more general initial / to path

--- lint.sh
+++ lint.sh
@@ -7,9 +7,6 @@ pwd=$(pwd)
 
 if [[ "$OS" == "Windows_NT" ]]; then
   docker_cmd="winpty docker"
-  if [[ "$MSYSTEM" == MINGW* ]]; then
-    pwd=`echo $pwd | sed 's/\//\/\//g'`
-  fi
 fi
 
-$docker_cmd run --rm -v $pwd/docs/:/src/ -v $pwd/docs/.proselintrc:/root/.proselintrc -it singapore/lint-condo:0.18.0
+$docker_cmd run --rm -v /$pwd/docs/:/src/ -v /$pwd/docs/.proselintrc:/root/.proselintrc -it singapore/lint-condo:0.18.0
