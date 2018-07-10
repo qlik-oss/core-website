9b567ccd8f3fffdf52acd0e9a83a6eb2dea81670
Jonas Hörström <Jonas.Horstrom@qlik.com>
2018-07-10 10:54:19 +0200

    Fix incorrect folder parsing in git bash (mingw64)

--- lint.sh
+++ lint.sh
@@ -7,6 +7,10 @@ pwd=$(pwd)
 
 if [[ "$OS" == "Windows_NT" ]]; then
   docker_cmd="winpty docker"
+  if [[ "$MSYSTEM" == MINGW* ]]; then
+    pwd=`echo $pwd | sed 's/\//\/\//g'`
+    echo $pwd
+  fi
 fi
 
 $docker_cmd run --rm -v $pwd/docs/:/src/ -v $pwd/docs/.proselintrc:/root/.proselintrc -it singapore/lint-condo:0.18.0
