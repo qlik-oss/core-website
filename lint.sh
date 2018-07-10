8b603cb2bd846e5df25791d1e926ab78da6b8716
Jonas Hörström <Jonas.Horstrom@qlik.com>
2018-07-10 14:42:14 +0200

    Using the same fix as in serve.sh

--- lint.sh
+++ lint.sh
@@ -7,6 +7,7 @@ pwd=$(pwd)
 
 if [[ "$OS" == "Windows_NT" ]]; then
   docker_cmd="winpty docker"
+  pwd=/$(pwd -W)
 fi
 
-$docker_cmd run --rm -v /$pwd/docs/:/src/ -v /$pwd/docs/.proselintrc:/root/.proselintrc -it singapore/lint-condo:0.18.0
+$docker_cmd run --rm -v $pwd/docs/:/src/ -v $pwd/docs/.proselintrc:/root/.proselintrc -it singapore/lint-condo:0.18.0
