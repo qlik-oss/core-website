372a4f4cca55d6fb45fe1d03fa27ddb524f93f7f
Aiham Azmeh <aaz@qlik.com>
2018-09-07 13:01:06 +0200

    ++ fixing cross platform build.sh(circleci)

--- build.sh
+++ build.sh
@@ -5,23 +5,21 @@ cd "$(dirname "$0")"
 echo "Building site into `pwd`/site/ folder."
 
 docker_cmd=docker
+find_cmd="sudo find"
+mv_cmd="sudo mv"
 pwd=$(pwd)
 
 if [[ "$OS" == "Windows_NT" ]]; then
   docker_cmd="winpty -Xallow-non-tty docker"
   pwd=/$(pwd -W)
+  find_cmd="find"
+  mv_cmd="mv"
 fi
 
 $docker_cmd run --rm -it --name mkdocs -v $pwd:/docs squidfunk/mkdocs-material:3.0.3 build
 
-find ./site/assets -type f -printf "%T@ %p\n" | sort -nr | cut -d\  -f2- > ./site/files.txt
-find ./site/images -type f -printf "%T@ %p\n" | sort -nr | cut -d\  -f2- >> ./site/files.txt
+$find_cmd ./site/assets -type f -printf "%T@ %p\n" | sort -nr | cut -d\  -f2- > files.txt
+$find_cmd ./site/images -type f -printf "%T@ %p\n" | sort -nr | cut -d\  -f2- >> files.txt
+$mv_cmd files.txt site/files.txt
 
 
-# CONTAINER_ID=$(docker ps -qf "name=mkdocs")
-# echo "image ID is $CONTAINER_ID"
-# $docker_cmd exec -it $CONTAINER_ID "sh ls -lha"  > filelist.txt
-# echo "docker should have execur´ted stuff.."
-# $docker_cmd kill $CONTAINER_ID 
-# $docker_cmd rm $CONTAINER_ID 
-# echo "docker killl and remove"
