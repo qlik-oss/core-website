7531a7f34a7200b8b9ea15a31d370319591c8b8d
Jonas Hörström <jonas.horstrom@gmail.com>
2020-07-03 15:50:26 +0200

    Deploy test of core-website-lite version

--- build-netlify.sh
+++ build-netlify.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-pip install --upgrade pip --quiet && pip install mkdocs-material==4.1.1 --quiet && mkdocs build && cp _redirects ./site
+#pip install --upgrade pip --quiet && pip install mkdocs-material==4.1.1 --quiet && mkdocs build && cp _redirects ./site
